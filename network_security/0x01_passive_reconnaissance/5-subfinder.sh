db01edeadcdad4e27987cf3b612f876c01eec253
parvizcode <gparvizn@gmail.com>
2025-08-16 14:57:23 +0000

    Add 5-subfinder.sh to fetch subdomains in Host,IP format

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d $1 -silent | while read host; do ip=$(dig +short $host | head -n1); [ ! -z "$ip" ] && echo "$host,$ip"; done > $1.txt
+subfinder -silent -d $1 -o $1.txt -nW -oI
