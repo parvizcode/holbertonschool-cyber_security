dbbe6800c014b6d6565bb5c69b0fa8a59f25080f
parvizcode <gparvizn@gmail.com>
2025-08-16 14:46:38 +0000

    Add script to retrieve all DNS records of a domain using dig

--- network_security/0x01_passive_reconnaissance/4-dig_all.sh
+++ network_security/0x01_passive_reconnaissance/4-dig_all.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-# Yoxlayırıq ki, domen argument olaraq verilib
-if [ -z "$1" ]; then
-    echo "Usage: $0 <domain>"
-    exit 1
-fi
-
-# dig ilə yalnız cavabları göstəririk
-dig +nocmd "$1" any +noall +answer
+dig +noall +answer any $1
