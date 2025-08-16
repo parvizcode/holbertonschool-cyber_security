8190564a79166503f8e2f893e94c9d33149dbe76
parvizcode <gparvizn@gmail.com>
2025-08-16 14:12:57 +0000

    Add bash script to extract whois info in CSV format using awk

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,23 +1,2 @@
 #!/bin/bash
-
-if [ -z "$1" ]; then
-    echo "Usage: $0 <domain>"
-    exit 1
-fi
-
-domain="$1"
-output_file="${domain}.csv"
-
-whois "$domain" | awk -F': ' -v domain="$domain" '
-    BEGIN { print "Type,Value" }
-    /Registrant|Admin|Tech/ && /Organization|State\/Province|Country|Email/ {
-        gsub(/^[ \t]+|[ \t]+$/, "", $1);
-        gsub(/^[ \t]+|[ \t]+$/, "", $2);
-        if ($1 ~ /Email/) {
-            print $1 "," $2 "https://domains.markmonitor.com/whois/" domain
-        } else {
-            print $1 "," $2
-        }
-    }' > "$output_file"
-
-echo "Whois data saved to $output_file"
+whois $1 | awk '/^(Registrant|Admin|Tech)/ {gsub(/^[ \t]+|[ \t]+$/, "", $0); split($0,a,":"); gsub(/^ /,"",a[2]); print a[1]","a[2]}' > $1.csv
